d420a9713d324cc06e5e45e2d4259143d46f3bd8
Casey Dailey <casey@feasttogether.org>
2017-08-05 14:15:57 -0500

    ex1 print is puts. cool.

---
+++ ex1/ex1.rb
@@ -0,0 +1,15 @@
+# 'print' Ruby is 'puts'
+# comments are denoted by the good ole "#" (octothorpe)
+
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "Id much rather you 'not'."
+puts 'I "said" do not touch this.'
+
+puts "--------------------"
+
+puts "Here's some new stuff."
+
