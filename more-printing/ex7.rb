5e7c07fc9e80703b24aad108c5699eeb232f08ec
Casey Dailey <casey@feasttogether.org>
2017-08-05 16:48:15 -0500

    more print and puts

---
+++ more-printing/ex7.rb
@@ -0,0 +1,32 @@
+puts "Mary had a little lamb."
+puts "Its fleece was white as #{'snow'}."
+puts "And everywhere that Mary went."
+
+#this prints 10 periods
+puts "." * 10 #what's this do?
+
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+
+#these will print on the same line
+#print and puts are both ways of printing,
+#the key difference is that puts adds a new line after executing,
+#and print does not
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
+
+
+
