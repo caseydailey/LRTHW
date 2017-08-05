5c54fafcbd59c208675d85fb6fb7bf5004d851fa
Casey Dailey <casey@feasttogether.org>
2017-08-05 14:44:05 -0500

    added table of operators by prevedence for reference

--- ex3/ex3.rb
+++ ex3/ex3.rb
@@ -33,3 +33,51 @@ puts "Is it less or equal? #{5 <= -2}"
 
 
 
+# I'm using "=begin" and '=end' here to denote a huge block comment.
+# the table list Ruby operators by precedence and is pulled from 
+# https://stackoverflow.com/questions/21060234/ruby-operator-precedence-table
+
+
+=begin
+
+The following table is ordered according to descending precedence (highest precedence at the top).
+
+N A M  Operator(s)            Description
+- - -  -----------            -----------
+1 R Y  ! ~ +                  boolean NOT, bitwise complement, unary plus
+                              (unary plus may be redefined from Ruby 1.9 with +@)
+
+2 R Y  **                     exponentiation
+1 R Y  -                      unary minus (redefine with -@)
+
+2 L Y  * / %                  multiplication, division, modulo (remainder)
+2 L Y  + -                    addition (or concatenation), subtraction
+
+2 L Y  << >>                  bitwise shift-left (or append), bitwise shift-right
+2 L Y  &                      bitwise AND
+
+2 L Y  | ^                    bitwise OR, bitwise XOR (exclusive OR)
+2 L Y  < <= >= >              ordering
+
+2 N Y  == === != =~ !~ <=>    equality, pattern matching, comparison
+                              (!= and !~ may not be redefined prior to Ruby 1.9)
+
+2 L N  &&                     boolean AND
+2 L N  ||                     boolean OR
+
+2 N N  .. ...                 range creation (inclusive and exclusive)
+                              and boolean flip-flops
+
+3 R N  ? :                    ternary if-then-else (conditional)
+2 L N  rescue                 exception-handling modifier
+
+2 R N  =                      assignment
+2 R N  **= *= /= %= += -=     assignment
+2 R N  <<= >>=                assignment
+2 R N  &&= &= ||= |= ^=       assignment
+
+1 N N  defined?               test variable definition and type
+1 R N  not                    boolean NOT (low precedence)
+2 L N  and or                 boolean AND, boolean OR (low precedence)
+
+=end
