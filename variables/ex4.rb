667c25f4c4d18b3b1d03edf3011a834107400c2d
Casey Dailey <casey@feasttogether.org>
2017-08-05 16:02:50 -0500

    clarified names of exercises

---
+++ variables/ex4.rb
@@ -0,0 +1,17 @@
+# variables in Ruby:
+
+#just like in python types are dynamic in ruby
+# x = 3 
+# 
+# type can change:
+# x = "hello" 
+#
+# and also as in python you cannot do this:
+# 
+# x = "3"
+# 
+# y = x + 5
+# 
+# because, like python, 
+# ruby is dynamically and strongly typed
+
