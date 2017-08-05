46c79aa91b27896fbfd91ef0cd1b07d4b16e990f
Casey Dailey <casey@feasttogether.org>
2017-08-05 16:25:01 -0500

    variables with strings

---
+++ more-variables/ex5.rb
@@ -0,0 +1,21 @@
+# as we noticed early on, you put expressions in strings
+# with the '${}' combination. 
+# this lesson is a review of that and an opportunity for a bit more practice
+
+my_name = 'Casey Dailey'
+my_age = 31
+my_height = 74 #inches
+my_weight = 150 #pounds
+my_eyes = 'Blue'
+my_teeth = '[kinda] White-ish'
+my_hair = 'Brown'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually, that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+
