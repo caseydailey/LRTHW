bb23a5e00184faf5e1ce90b1cb9dd97bf734e6af
Casey Dailey <casey@feasttogether.org>
2017-08-05 17:27:50 -0500

    excape sequences and more printing of strings

---
+++ escape-sequences/ex10.rb
@@ -0,0 +1,48 @@
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+
+# just like in python the """ 
+# basically opens a block multiline string
+# we saw this in the previous exercise as '%q{}'
+
+fat_cat = """
+
+I'll do a list:
+\t* Cat food
+\t* Fishies 
+|t* Catnip\n\t* Grass
+
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
+
+
+#here's a list of some other excape sequences ruby supports
+
+=begin  **** '=begin' starts a block comment.
+    
+Escape      What it does.
+
+\\          Backslash ()
+\'          Single-quote (')
+\"          Double-quote (")
+\a          ASCII bell (BEL)
+\b          ASCII backspace (BS)
+\f          ASCII formfeed (FF)
+\n          ASCII linefeed (LF)
+\r          ASCII Carriage Return (CR)
+\t          ASCII Horizontal Tab (TAB)
+\uxxxx      Character with 16-bit hex value xxxx (Unicode only)
+\v          ASCII vertical tab (VT)
+\ooo        Character with octal value ooo
+\xhh        Character with hex value hh
+
+
+=end # '=end' ends a block comment
