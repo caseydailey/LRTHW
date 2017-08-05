878279bab92ed25a720edef02169918b2d2b679b
Casey Dailey <casey@feasttogether.org>
2017-08-05 18:02:51 -0500

    ARGV for user input

---
+++ user-input-ARGV/ex12.rb
@@ -0,0 +1,9 @@
+# lets write a script that accepts arguments 
+
+# take whatever is in ARGV and unpacks into
+# these variables just like in python
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
