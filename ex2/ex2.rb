344a6a3b7f4fd9e38ea2884d821c4d3fa9c399f2
Casey Dailey <casey@feasttogether.org>
2017-08-05 14:20:26 -0500

    ex2 covers the ocothorpe for comments. cool.

---
+++ ex2/ex2.rb
@@ -0,0 +1,8 @@
+# as mentioned in the previous exercise,
+# the '#' octothorpe, pund, has, whatever
+# denotes a comment in Ruby, just like in python
+
+# example. this line will not run
+
+puts "this line will."
+
