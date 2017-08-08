0cf4244cef53c4174f477e7fcbacb4806240b84d
Casey Dailey <casey@feasttogether.org>
2017-08-08 16:49:52 -0500

    opening reading and closing a txt file

--- read-files/ex15.rb
+++ read-files/ex15.rb
@@ -8,6 +8,8 @@ txt = open(filename);
 puts "Here's your file #{filename}:"
 print txt.read
 
+txt.close()
+
 # $stdin.gets.chomp gets the user's input
 print "Type the filename again: "
 file_again = $stdin.gets.chomp
@@ -15,3 +17,5 @@ file_again = $stdin.gets.chomp
 txt_again = open(file_again)
 
 print txt_again.read
+
+txt_again.close()
