c76b5c0ce5e882d74e519081a41e559a87f71617
Casey Dailey <casey@feasttogether.org>
2017-08-16 08:34:04 -0500

    oops

--- more-read-and-write/ex17.rb
+++ more-read-and-write/ex17.rb
@@ -11,7 +11,6 @@ puts "Copying from #{from_file} to #{to_file}."
 
 in_file = open(from_file)
 indata = in_file.read
-
 puts "The input file is #{indata.length} bytes long"
 
 puts "Does the output file exist? #{File.exist?(to_file)}"
