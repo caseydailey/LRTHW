71e481ed46588bc2e375b0ded4b01a5dff006efc
Casey Dailey <casey@feasttogether.org>
2017-08-18 07:24:34 -0500

    oops

--- functions/ex18.rb
+++ functions/ex18.rb
@@ -14,15 +14,17 @@ end
 
 
 def print_one(arg1)
-    puts "arg1: #{arg1}, arg2: #{arg2}"
+    puts "arg1: #{arg1}"
 end
 
 def print_none()
     puts "I got nothin'."
 end
 
+
+
 print_two("Casey", "Dailey")
 print_two_again("Casey", "Dailey")
 print_one("First!")
-ptint_none()
+print_none()
 
